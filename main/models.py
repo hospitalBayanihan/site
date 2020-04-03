7e316bac9c54b97393a1baeacd15352fba603d74
hrbenitez <herlan.benitez@gmail.com>
2020-04-03 16:33:27 +0800

    Added other tables to database and models

--- main/models.py
+++ main/models.py
@@ -4,19 +4,13 @@ from airtable import Airtable
 
 # Create your models here.
 
-def getFields():
-    # Function to get fields from airtable
-    atable = Airtable('appLsmHfyJGeiXffC', 'Hospitals', api_key='keys8DEQxXELZX13q')
-    
-    # Template entry exists where all columns are filled
-    # Unfilled airtable fields do not show as a field
-    tmp = atable.match('Hospital Name', 'Template') 
-    return (t for t in tmp['fields'])
-    
-fields = getFields()
-
 class Hospitals(models.Model):
     id = models.TextField(primary_key=True)  # AutoField?
+    
+    atable = Airtable('appLsmHfyJGeiXffC', 'Hospitals')
+    tmp = atable.match('Name', 'Template - Template') 
+    
+    fields = (t for t in tmp['fields'])
     
     # List of fields that will be integer fields
     to_int = (
@@ -26,7 +20,7 @@ class Hospitals(models.Model):
     
     for f in fields:
         # Replace spaces with '_' and remove '%' symbols
-        t = f.replace(' ', '_').replace('%', '').lower()
+        t = f.replace(' ', '_').replace('%', '').replace('?', '').lower()
         
         # locals()[t] creates a variable with name contained in string t
         if t.find('weekly_volume') > -1 or t in to_int:
@@ -36,9 +30,124 @@ class Hospitals(models.Model):
     
     def __str__(self):
         tmp = ""
-        for f in fields:
+        for f in self.fields:
             tmp += f + ' : ' + getattr(self, f) + '\n'
         return tmp
     
     class Meta:
         db_table = 'Hospitals'
+        
+class Updates(models.Model):
+    id = models.TextField(primary_key=True)  # AutoField?
+    hospital = models.ForeignKey(Hospitals, on_delete=models.CASCADE)
+    
+    atable = Airtable('appLsmHfyJGeiXffC', 'Update Data')
+    tmp = atable.match('Hospital', 'Template - Template') 
+    
+    fields = (t for t in tmp['fields'])
+    
+    # List of fields that will be integer fields
+    to_int = (
+        'number_of_active_cases',
+        'average_personnel_count',
+    )
+    
+    to_skip = (
+        'hospital',
+    )
+    
+    for f in fields:
+        # Replace spaces with '_' and remove '%' symbols
+        t = f.replace(' ', '_').replace('%', '').replace('?', '').lower()
+        
+        # locals()[t] creates a variable with name contained in string t
+        if t in to_skip:
+            continue
+        elif t.find('weekly_volume') > -1 or t in to_int:
+            locals()[t] = models.IntegerField(blank=True, null=True)
+        else:
+            locals()[t] = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        tmp = ""
+        for f in self.fields:
+            tmp += f + ' : ' + getattr(self, f) + '\n'
+        return tmp
+    
+    class Meta:
+        db_table = 'Updates'
+        
+class Requests(models.Model):
+    id = models.TextField(primary_key=True)  # AutoField?
+    hospital = models.ForeignKey(Hospitals, on_delete=models.CASCADE)
+    
+    atable = Airtable('appLsmHfyJGeiXffC', 'Weekly Supply Tracker')
+    tmp = atable.match('Hospital', 'Template - Template') 
+    
+    fields = (t for t in tmp['fields'])
+    
+    to_include = (
+        'covered_week',
+        'request_status',
+        'deadline',
+        'overdue',
+    )
+    
+    for f in fields:
+        # Replace spaces with '_' and remove '%' symbols
+        t = f.replace(' ', '_').replace('%', '').replace('?', '').lower()
+        
+        # locals()[t] creates a variable with name contained in string t
+        if t in to_include:
+            locals()[t] = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        tmp = ""
+        for f in self.fields:
+            tmp += f + ' : ' + getattr(self, f) + '\n'
+        return tmp
+    
+    class Meta:
+        db_table = 'Requests'
+
+class Fulfillments(models.Model):
+    id = models.TextField(primary_key=True)  # AutoField?
+    request = models.ForeignKey(Requests, on_delete=models.CASCADE)
+    
+    atable = Airtable('appLsmHfyJGeiXffC', 'Fulfillment')
+    tmp = atable.match('Donated by', 'Template') 
+    
+    fields = (t for t in tmp['fields'])
+    
+    # List of fields that will be integer fields
+    to_int = (
+        'number_of_active_cases',
+        'personnel_count_for_food',
+    )
+    
+    to_skip = (
+        'hospital',
+        'request_fulfilled',
+    )
+    
+    for f in fields:
+        # Replace spaces with '_' and remove '%' symbols
+        t = f.replace(' ', '_').replace('%', '').replace('?', '').lower()
+        
+        # locals()[t] creates a variable with name contained in string t
+        if t in to_skip:
+            continue
+        if t.find('provided') > -1 or t in to_int:
+            locals()[t] = models.IntegerField(blank=True, null=True)
+        else:
+            locals()[t] = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        tmp = ""
+        for f in self.fields:
+            tmp += f + ' : ' + getattr(self, f) + '\n'
+        return tmp
+    
+    class Meta:
+        db_table = 'Fulfillments'
+        
--- main/views.py
+++ main/views.py
@@ -8,7 +8,7 @@ from django.views.generic import (
     UpdateView,
     DeleteView
 )
-from .models import Hospitals
+from .models import Hospitals, Updates, Requests, Fulfillments
 
 from airtable import Airtable
 
@@ -27,23 +27,28 @@ def main(request):
 
 def refresh(request):
     # View to update sqlite db with airtable data
-
-    atable = Airtable('appLsmHfyJGeiXffC', 'Hospitals', api_key='keys8DEQxXELZX13q')
-
+    tables = (
+        'Hospitals',
+        'Update Data',
+        'Weekly Supply Tracker',
+        'Fulfillment',
+    )
+    
+    table_key = 'appLsmHfyJGeiXffC' # DEV table
+    
+    atable = Airtable(table_key, 'Hospitals')
     # Fields to skip
+  
+    #-- Update Hospitals Table --#
     skip = (
         'hospital_id_of_submitting_person',
     )
-    
-    # hospitals contains a list of each row in from airtable
-    hospitals = atable.get_all(view='Raw Hospital Data')
-    
+    hospitals = atable.get_all(view='Raw Hospital Data')  
     for hosp in hospitals:
         hospitals_inputs = Hospitals()   
         hospitals_inputs.id = hosp['id']
         for f in hosp['fields']:
-            # hosp['fields'] is a dictionary where keys are fields of airtable and values have the corresponding value
-            t = f.replace(' ', '_').replace('%', '').lower()
+            t = f.replace(' ', '_').replace('%', '').replace('?', '').lower()
             
             if t in skip:
                 continue
@@ -53,5 +58,66 @@ def refresh(request):
             
         # Save each hosp data stored in hospitals_inputs to db
         hospitals_inputs.save()
-
+    #-- End Update Hospitals Table --#
+    
+    #-- Update Updates Table --#
+    atable = Airtable(table_key, 'Update Data')
+    
+    updates = atable.get_all(view='Grid view')  
+    for update in updates:
+        updates_inputs = Updates()   
+        updates_inputs.id = update['id']
+        updates_inputs.hospital = Hospitals.objects.get(id=update['fields']['Hospital'][0])
+        
+        for f in update['fields']:
+            t = f.replace(' ', '_').replace('%', '').replace('?', '').lower()
+            try:
+                if t in Updates.to_skip:
+                    continue
+                setattr(updates_inputs, t, update['fields'][f])
+            except:
+                continue
+        updates_inputs.save()
+    #-- End Update Updates Table --#
+    
+    #-- Update Requests Table --#
+    atable = Airtable(table_key, 'Weekly Supply Tracker')
+    
+    requests = atable.get_all(view='Grid 3')  
+    for req in requests:
+        requests_inputs = Requests()   
+        requests_inputs.id = req['id']
+        
+        requests_inputs.hospital = Hospitals.objects.get(id=req['fields']['Hospital'][0])
+        
+        for f in req['fields']:
+            t = f.replace(' ', '_').replace('%', '').replace('?', '').lower()
+            
+            if t in Requests.to_include:
+                setattr(requests_inputs, t, req['fields'][f])
+        
+        requests_inputs.save()
+    #-- End Update Requests Table --#
+    
+    
+    #-- Update Fulfillments Table --#
+    atable = Airtable(table_key, 'Fulfillment')
+    
+    fulfillments = atable.get_all(view='Grid view')  
+    for ful in fulfillments:
+        fulfillments_inputs = Fulfillments()   
+        fulfillments_inputs.id = ful['id']
+        fulfillments_inputs.request = Requests.objects.get(id=ful['fields']['Request Fulfilled'][0])
+        
+        for f in ful['fields']:
+            t = f.replace(' ', '_').replace('%', '').replace('?', '').lower()
+            try:
+                if t in Fulfillments.to_skip:
+                    continue
+                setattr(fulfillments_inputs, t, ful['fields'][f])
+            except:
+                continue
+        fulfillments_inputs.save()
+    #-- End Update Fulfillments Table --#
+    
     return render(request, 'main/index.html', {'title': 'Insert Tagline Here'})
